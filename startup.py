488fb939b9786a38500b8ff650c34060789dfa9b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-18 00:25:38 +0200

    Pretty output for nat.giuridica field.

--- startup.py
+++ startup.py
@@ -5,23 +5,14 @@ from collections import Counter
 import pandas as pd
 
 XLS_NAME = 'startup.xls'
-SHEET_NAME = 'STARTUP_08092014'
+SHEET_NAME = 'STARTUP_15092014'
+COL_NAME = 'nat.giuridica'
 
 def main():
     xls = pd.ExcelFile(XLS_NAME)
     sheet = xls.parse(SHEET_NAME, index_col=None)
-    cols = [col for col in sheet]
-
-    print Counter(sheet[cols[1]])
-    print
-    print Counter(sheet[cols[3]])
-    print Counter(sheet[cols[4]])
-    print
-    print Counter(sheet[cols[9]])
-    print Counter(sheet[cols[10]])
-    print
-    print Counter(sheet[cols[11]])
-    print Counter(sheet[cols[12]])
+    for k,v in Counter(sheet[COL_NAME]).most_common():
+	    print "%4d\t%s" % (v, k)
 
 if __name__ == '__main__':
     main()
