da2e14bb6a63ef10291e61856a6a39354302ae68
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-18 10:33:10 +0200

    Use a few constants.

--- startup.py
+++ startup.py
@@ -7,21 +7,26 @@ import pandas as pd
 XLS_NAME = 'startup.xls'
 SHEET_NAME = 'STARTUP_15092014'
 
+BUSINESS_TYPE = 'nat.giuridica'
+REVENUE_CLASS = 'classe di valore della produzione ultimo anno (1)'
+EMPLOYEE_CLASS = 'classe di addetti ultimo anno (2)'
+CLASSES = ['A', 'B', 'C', 'D', 'E']
+
 def main():
     xls = pd.ExcelFile(XLS_NAME)
-    sheet = xls.parse(SHEET_NAME, index_col=None, convert_float=False)
+    sheet = xls.parse(SHEET_NAME)
 
-    data = [el for el in sheet['nat.giuridica']]
+    data = [el for el in sheet[BUSINESS_TYPE]]
     for k,v in Counter(data).most_common():
         print "%4d\t%s" % (v, k)
     print
 
-    data = [el for el in sheet['classe di valore della produzione ultimo anno (1)'] if el in ['A', 'B', 'C', 'D', 'E']]
+    data = [el for el in sheet[REVENUE_CLASS] if el in CLASSES]
     for k,v in Counter(data).most_common():
         print "%4d\t%s" % (v, k)
     print
 
-    data = [el for el in sheet['classe di addetti ultimo anno (2)'] if el in ['A', 'B', 'C', 'D', 'E']]
+    data = [el for el in sheet[EMPLOYEE_CLASS] if el in CLASSES]
     for k,v in Counter(data).most_common():
         print "%4d\t%s" % (v, k)
 
