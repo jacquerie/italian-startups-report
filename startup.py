d5eccc801634f1b841fbc31de545e530b6d4bd54
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-18 02:46:07 +0200

    Add pretty output for two more fields.

--- startup.py
+++ startup.py
@@ -6,13 +6,25 @@ import pandas as pd
 
 XLS_NAME = 'startup.xls'
 SHEET_NAME = 'STARTUP_15092014'
-COL_NAME = 'nat.giuridica'
 
 def main():
     xls = pd.ExcelFile(XLS_NAME)
-    sheet = xls.parse(SHEET_NAME, index_col=None)
-    for k,v in Counter(sheet[COL_NAME]).most_common():
-	    print "%4d\t%s" % (v, k)
+    sheet = xls.parse(SHEET_NAME, index_col=None, convert_float=False)
+
+    data = [el for el in sheet['nat.giuridica']]
+    for k,v in Counter(data).most_common():
+        print "%4d\t%s" % (v, k)
+    print
+
+    data = [el for el in sheet['classe di valore della produzione ultimo anno (1)'] if el in ['A', 'B', 'C', 'D', 'E']]
+    for k,v in Counter(data).most_common():
+        print "%4d\t%s" % (v, k)
+    print
+
+    data = [el for el in sheet['classe di addetti ultimo anno (2)'] if el in ['A', 'B', 'C', 'D', 'E']]
+    for k,v in Counter(data).most_common():
+        print "%4d\t%s" % (v, k)
+
 
 if __name__ == '__main__':
     main()
