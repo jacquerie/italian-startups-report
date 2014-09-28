d704114c1aa211a8f22552990b06e91f2d295f70
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-28 15:21:26 +0200

    Make program PEP8 compliant.

--- startup.py
+++ startup.py
@@ -13,28 +13,51 @@ EMPLOYEE_CLASS = 'classe di addetti ultimo anno (2)'
 
 CLASSES = ['A', 'B', 'C', 'D', 'E']
 
+
 def main():
     xls = pd.ExcelFile(XLS_NAME)
     sheet = xls.parse(SHEET_NAME)
 
+    # Business type counts.
     print sheet[BUSINESS_TYPE].value_counts()
     print
 
-    print sheet[sheet[REVENUE_CLASS].isin(CLASSES)][REVENUE_CLASS].value_counts()
+    # Revenue classes counts.
+    print sheet[
+        sheet[REVENUE_CLASS].isin(CLASSES)
+    ][REVENUE_CLASS].value_counts()
     print
 
-    print sheet[sheet[EMPLOYEE_CLASS].isin(CLASSES)][EMPLOYEE_CLASS].value_counts()
+    # Employee classes counts.
+    print sheet[
+        sheet[EMPLOYEE_CLASS].isin(CLASSES)
+    ][EMPLOYEE_CLASS].value_counts()
     print
 
-    print sheet[(sheet[REVENUE_CLASS].isin(CLASSES)) & (sheet[EMPLOYEE_CLASS].isin(CLASSES))].T.apply(lambda el: el[REVENUE_CLASS] + el[EMPLOYEE_CLASS]).value_counts()
+    # Classes mixing revenue class and employee class, and their counts.
+    print sheet[
+        (sheet[REVENUE_CLASS].isin(CLASSES)) &
+        (sheet[EMPLOYEE_CLASS].isin(CLASSES))
+    ].T.apply(
+        lambda el: el[REVENUE_CLASS] + el[EMPLOYEE_CLASS]
+    ).value_counts()
     print
 
     # Startups whose revenue class is smaller than their employee class.
-    print sheet.loc[(sheet[REVENUE_CLASS].isin(CLASSES)) & (sheet[EMPLOYEE_CLASS].isin(CLASSES)) & (sheet[REVENUE_CLASS] < sheet[EMPLOYEE_CLASS]), BUSINESS_NAME]
+    print sheet.loc[
+        (sheet[REVENUE_CLASS].isin(CLASSES)) &
+        (sheet[EMPLOYEE_CLASS].isin(CLASSES)) &
+        (sheet[REVENUE_CLASS] < sheet[EMPLOYEE_CLASS]), BUSINESS_NAME]
     print
 
     # Startups whose revenue class is much bigger than their employee class.
-    print sheet.loc[sheet.index[sheet[(sheet[REVENUE_CLASS].isin(CLASSES)) & (sheet[EMPLOYEE_CLASS].isin(CLASSES))].T.apply(lambda el: ord(el[REVENUE_CLASS]) > ord(el[EMPLOYEE_CLASS]) + 1).T], BUSINESS_NAME]
+    print sheet.loc[
+        sheet.index[sheet[
+            (sheet[REVENUE_CLASS].isin(CLASSES)) &
+            (sheet[EMPLOYEE_CLASS].isin(CLASSES))
+        ].T.apply(
+            lambda el: ord(el[REVENUE_CLASS]) > ord(el[EMPLOYEE_CLASS]) + 1).T
+        ], BUSINESS_NAME]
     print
 
 
