2f3f4756500a78a72ec41aa6f993dfbcd862cb2b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-12-15 11:56:36 +0100

    Update for new data.

--- report.py
+++ report.py
@@ -5,7 +5,7 @@ import itertools
 import math
 import pandas as pd
 
-DATE = '2014-12-08'
+DATE = '2014-12-15'
 XLS_NAME = 'startup.xls'
 SHEET_NAME = 'STARTUP_' + ''.join(reversed(DATE.split('-')))
 
