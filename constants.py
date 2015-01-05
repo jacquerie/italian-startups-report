1beb7340cbd58fadebb9e75df92b302e624cef7b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2015-01-05 21:58:37 +0100

    Update for new data.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-DATE = '2014-12-29'
+DATE = '2015-01-05'
 XLS_NAME = 'startup.xls'
 SHEET_NAME = 'STARTUP_' + ''.join(reversed(DATE.split('-')))
 
